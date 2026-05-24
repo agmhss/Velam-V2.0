2d7ba4746e8b7523a671cec5728a2cc4100ff3fe
AGMHSS <agmhsspatteeswaram@gmail.com>
2026-05-24 05:53:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 const APP_CONFIG = {
     fullName: "GHSS VELAMURITHANPETTAI", 
     shortName: "GHSS VMPT",                                           
-    scriptUrl: "https://script.google.com/macros/s/AKfycbxxOOe8vMBl4VpDS1yqv1DyR1GIlL_d6-J10fe99FZT69H4YIQDVNTojUosHCYJDxGn/exec" 
+    scriptUrl: "https://script.google.com/macros/s/AKfycbwhoxfOTBzWYD7hJyFP2mI8-cfJS77npL4SyAd6N1Ps2RUw29oIHffGejX_hkAjxmWW4g/exec" 
 };
 const SCRIPT_URL = APP_CONFIG.scriptUrl;
 
